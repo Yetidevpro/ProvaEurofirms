5442c9cb3d6498afe11b77c997fd5e6567ac093e
Albert Alarcón Pascual <albertdevpro@gmail.com>
2025-05-13 21:24:55 +0200

    Crear Projecte Domain amb el seu Model

---
+++ PruebaEurofirms.Domain/Models/Character.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace PruebaEurofirms.Domain.Models
+{
+    public class Character
+    {
+        [Key]
+        public int CharacterId { get; set; }
+
+        [Required]
+        [StringLength(100)]
+        public string Name { get; set; }
+
+        [Required]
+        [StringLength(100)]
+        public string Status { get; set; }
+
+        [Required]
+        [StringLength(100)]
+        public string Gender { get; set; }
+
+        [Required]
+        public List<string> Episodes { get; set; } = new List<string>();
+    }
+}
