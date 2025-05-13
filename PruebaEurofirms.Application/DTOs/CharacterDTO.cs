3ef83be1d2254211d0e56b2608c459df47346cf0
Albert Alarcón Pascual <albertdevpro@gmail.com>
2025-05-13 21:36:48 +0200

    Crear Proyecto Application, referenciar con Domain y crear Carpeta DTOs

---
+++ PruebaEurofirms.Application/DTOs/CharacterDTO.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PruebaEurofirms.Application.DTOs
+{
+    public class CharacterDTO
+    {
+        public int CharacterId { get; set; }
+        public string Name { get; set; }
+        public string Status { get; set; }
+        public string Gender { get; set; }
+        public List<string> Episodes { get; set; } = new List<string>();
+    }
+}
