3c5c037de6248bfad78707d278899a6da10b1df3
Albert Alarcón Pascual <albertdevpro@gmail.com>
2025-05-13 21:54:47 +0200

    creada Interfas a Domain per fer la primera prova de simulació de la base de dades en memoria

---
+++ PruebaEurofirms.Domain/Repositories/ICharacterRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PruebaEurofirms.Domain.Models;
+
+namespace PruebaEurofirms.Domain.Repositories
+{
+    public interface ICharacterRepository
+    {
+        Task<IEnumerable<Character>> GetCharactersByStatusAsync(string status);
+        Task<bool> SaveCharacterAsync(IEnumerable<Character> character);
+        Task<bool> DeleteCharacterAsync(int id);
+        Task<Character> GetCharacterByIdAsync(int id);
+    }
+}
